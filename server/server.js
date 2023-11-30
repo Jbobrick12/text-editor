d415bb04eaf71b7bbddc7d850237fe38ffae0d07
Justin Bobrick <j.bobrick@outlook.com>
2023-11-29 16:59:20 -0800

    added server js code

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+require('./routes/htmlRoutes')(app);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
