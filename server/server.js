6db9788f1606e86945a5f8b2404af36e105b4421
Justin Bobrick <j.bobrick@outlook.com>
2023-11-29 17:09:49 -0800

    server js code

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-require('./routes/htmlRoutes')(app);
+require("./routes/htmlRoutes")(app);
 
 // Start server
 app.listen(PORT, () => {
